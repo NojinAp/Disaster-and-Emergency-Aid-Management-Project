2578c4053650627228093e37b969b81b9388e655
Nojin <nojinap@gmail.com>
2022-04-30 16:47:33 -0400

    Update Utils.cpp

--- Utils.cpp
+++ Utils.cpp
@@ -2,9 +2,6 @@
     ==================================================
     Disaster and Emergency Aid Management Project
     ==================================================
-    Name   : Nozhin Azarpanah
-    Date   : April 11, 2022
-
 */
 
 #define _CRT_SECURE_NO_WARNINGS
